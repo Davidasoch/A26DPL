d4f30526938cc763b7e58b43986313ab546c23d0
David <davidac@vps638309.ovh.net>
2019-02-20 19:32:13 +0100

    date modified

--- routes/date.js
+++ routes/date.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Deploy automatico en heroku - Deploy manual provisional');
+  res.send('Deploy automatico en heroku - Deploy automatico funcionando');
 });
 
 module.exports = router;
